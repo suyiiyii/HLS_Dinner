fe393ae4900fbbc2625778b36d1a71e443b71db9
suyiiyii <suyiiyii@gmail.com>
2024-03-18 18:32:13 +0800

    更新工具库，提供直接面向特定的对象的操作方法

--- src/main/java/top/suyiiyii/Utils.java
+++ src/main/java/top/suyiiyii/Utils.java
@@ -4,10 +4,12 @@
  * 封装了几个经常要用到的方法，比如读取请求体和写响应体
  *
  * @auther Suyiiyii
- * @date 2023.3.17
+ * @date 2023.3.18
+ * @version 1.1
  */
 package top.suyiiyii;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -19,9 +21,9 @@ public class Utils {
     /**
      * 读取请求体（json格式），返回字符串
      *
-     * @param req
+     * @param req HttpServletRequest
      * @return String
-     * @throws IOException
+     * @throws IOException IOException
      */
     public static String readRequestBody(HttpServletRequest req) throws IOException {
         BufferedReader reader = req.getReader();
@@ -34,16 +36,37 @@ public class Utils {
     }
 
     /**
+     * 读取请求体（json格式），返回对象
+     *
+     * @param req       HttpServletRequest
+     * @param valueType Class<T>
+     * @param <T>       T
+     * @return T
+     * @throws IOException IOException
+     */
+    public static <T> T readRequestBody2Obj(HttpServletRequest req, Class<T> valueType) throws IOException {
+        BufferedReader reader = req.getReader();
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+        String str = stringBuilder.toString();
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(str, valueType);
+    }
+
+    /**
      * 写响应体（json格式）
      * <p>
-     * 把json字符串写入响应体的body中，并设置响应头的Content-Type为application/json
+     * 把传入的对象转换成json字符串并写入到响应体，设置响应头的Content-Type为application/json
      *
-     * @param resp
-     * @param json
-     * @throws IOException
+     * @param resp   HttpServletResponse
+     * @param object Object
+     * @throws IOException IOException
      */
 
-    public static void respWrite(HttpServletResponse resp, String json) throws IOException {
+    public static void respWrite(HttpServletResponse resp, Object object) throws IOException {
         // 设置允许跨域的响应头
         resp.setHeader("Access-Control-Allow-Origin", "*"); // 允许任何域名发起请求，也可以指定具体的域名
         resp.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); // 允许的HTTP方法
@@ -52,7 +75,10 @@ public class Utils {
 
         resp.setContentType("application/json");
         PrintWriter pw = resp.getWriter();
-        pw.write(json);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonStr = mapper.writeValueAsString(object);
+        pw.write(jsonStr);
         pw.flush();
     }
 }
