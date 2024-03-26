118f6450f6e8c1fbe037b3bd40caffa3b5adf68b
suyiiyii <suyiiyii@gmail.com>
2024-03-27 00:19:09 +0800

    refactor: 使用orm操作重构UserTempImlp

--- src/main/java/top/suyiiyii/dao/UserTempImpl.java
+++ src/main/java/top/suyiiyii/dao/UserTempImpl.java
@@ -5,22 +5,39 @@ import org.apache.commons.logging.LogFactory;
 import top.suyiiyii.Su.orm.core.Session;
 import top.suyiiyii.models.User;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 public class UserTempImpl implements Users {
-    private static final List<User> users = new LinkedList<>();
+    Log logger = LogFactory.getLog(UserTempImpl.class);
+    private Session db;
+
+    public UserTempImpl(Session db) {
+        this.db = db;
+    }
 
     @Override
     public User getUserById(int id) {
-        return users.stream().filter(user1 -> user1.uid == id).findFirst().orElse(null);
+        logger.debug("getUserById: " + id);
+        User user = null;
+        try {
+            user = db.query(User.class).eq("id", id).first();
+        } catch (Exception e) {
+            logger.warn(e);
+        }
+        if (user == null) {
+            throw new NoSuchElementException("用户不存在");
+        }
+        return user;
     }
 
     @Override
     public User getUserByUsername(String username) throws NoSuchElementException {
-        User user = users.stream().filter(user1 -> user1.username.equals(username)).findFirst().orElse(null);
+        User user = null;
+        try {
+            user = db.query(User.class).eq("username", username).first();
+        } catch (Exception e) {
+            logger.warn(e);
+        }
         if (user == null) {
             throw new NoSuchElementException("用户不存在");
         }
@@ -29,8 +46,11 @@ public class UserTempImpl implements Users {
 
     @Override
     public User createUser(User user) {
-        user.uid = users.size() + 1;
-        users.add(user);
+        try {
+            db.insert(user);
+        } catch (Exception e) {
+            logger.warn(e);
+        }
         return user;
     }
 }
