6646bb09ea3a7326286341b96b076995f208659a
suyiiyii <suyiiyii@gmail.com>
2024-03-27 13:33:01 +0800

    fix(DI): 将注入的时机由init改为service

--- src/main/java/top/suyiiyii/su/servlet/BaseHttpServlet.java
+++ src/main/java/top/suyiiyii/su/servlet/BaseHttpServlet.java
@@ -3,9 +3,14 @@ package top.suyiiyii.su.servlet;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import top.suyiiyii.schemas.TokenData;
 import top.suyiiyii.su.ConfigManger;
 import top.suyiiyii.su.orm.core.ModelManger;
 import top.suyiiyii.su.orm.core.Session;
+
+import java.io.IOException;
 
 /**
  * BaseHttpServlet
@@ -16,13 +21,27 @@ import top.suyiiyii.su.orm.core.Session;
 public class BaseHttpServlet extends HttpServlet {
     protected Session db;
     protected ConfigManger configManger;
-    protected String username;
+    protected int uid;
 
+    /**
+     * 依赖注入
+     * 注意： ServletConfig是全局上下文，用于保存全局信息；而HttpServletRequest是请求上下文，用于保存请求信息
+     * init方法是在servlet初始化时调用的，service方法是在每次请求时调用的
+     *
+     * @param req  the {@link HttpServletRequest} object that contains the request the client made of the servlet
+     * @param resp the {@link HttpServletResponse} object that contains the response the servlet returns to the client
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletConfig config = getServletConfig();
         this.db = ((ModelManger) config.getServletContext().getAttribute("ModelManger")).getSession();
         this.configManger = (ConfigManger) config.getServletContext().getAttribute("ConfigManger");
-
+        TokenData tokenData = (TokenData) req.getAttribute("tokenData");
+        if (tokenData != null) {
+            this.uid = tokenData.uid;
+        }
+        super.service(req, resp);
     }
 }
