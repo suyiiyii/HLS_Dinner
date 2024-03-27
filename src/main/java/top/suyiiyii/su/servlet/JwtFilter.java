ad362161cde97b3b865d728db9df0710eb3322bc
suyiiyii <suyiiyii@gmail.com>
2024-03-27 14:06:04 +0800

    fix(auth): secret现在从config中获取

--- src/main/java/top/suyiiyii/su/servlet/JwtFilter.java
+++ src/main/java/top/suyiiyii/su/servlet/JwtFilter.java
@@ -4,6 +4,9 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import top.suyiiyii.schemas.TokenData;
+import top.suyiiyii.su.ConfigManger;
+import top.suyiiyii.su.UniversalUtils;
 
 import java.io.IOException;
 
@@ -12,16 +15,18 @@ import static top.suyiiyii.su.JwtUtils.verifyToken;
 
 /**
  * jwt 校验过滤器
- * 如果校验成功，注入tokenData到servletRequest
+ * 如果校验成功，注入tokenData到servletRequest上下文
  *
  * @author suyiiyii
  */
 public class JwtFilter implements Filter {
     Log logger = LogFactory.getLog(JwtFilter.class);
+    ConfigManger configManger;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Filter.super.init(filterConfig);
+        configManger = (ConfigManger) filterConfig.getServletContext().getAttribute("ConfigManger");
     }
 
     @Override
@@ -33,9 +38,11 @@ public class JwtFilter implements Filter {
             throw new ServletException("缺少Authorization头或者Authorization头不以Bearer开头");
         }
         String token = authHeader.substring(7);
-        //验证token
-        String tokenData = verifyToken(token, "suyiiiiiiiiiii");
-        if (!tokenData.isEmpty()) {
+        String tokenStr = verifyToken(token, configManger.get("secret"));
+        // 验证token
+        if (!tokenStr.isEmpty()) {
+            // 注入tokenData
+            TokenData tokenData = UniversalUtils.json2Obj(tokenStr, TokenData.class);
             req.setAttribute("tokenData", tokenData);
             logger.info("token verify success");
             filterChain.doFilter(servletRequest, servletResponse);
