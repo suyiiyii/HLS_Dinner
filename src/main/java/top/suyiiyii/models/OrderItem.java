89073204297cf04f77ba304964d0b2c27f37d123
suyiiyii <suyiiyii@gmail.com>
2024-03-30 15:57:17 +0800

    feat(Order): 添加订单表

---
+++ src/main/java/top/suyiiyii/models/OrderItem.java
@@ -0,0 +1,20 @@
+package top.suyiiyii.models;
+
+import top.suyiiyii.su.orm.annotation.ColumnSetting;
+import top.suyiiyii.su.orm.annotation.TableRegister;
+
+/**
+ * 订单商品关系表
+ * 存储一个订单中的商品信息
+ *
+ * @author suyiiyii
+ */
+@TableRegister("order_item")
+public class OrderItem {
+    @ColumnSetting(isPrimaryKey = true, isAutoIncrement = true)
+    public int id;
+    public int order_id;
+    public int dish_id;
+    public int quantity;
+    public int price;
+}
